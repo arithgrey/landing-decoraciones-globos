c110961da0e33a098fb97fe81b40764dc3a82e86
arihgrey@gmail.com <arihgrey@gmail.com>
2023-07-14 15:48:14 -0600

    [FEAT] temática de barbie

---
+++ barbie.php
@@ -0,0 +1,397 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta property="og:image" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+    <meta name="description" content="¡Llenaremos de magia y color tu evento! Decoraciones temáticas con globos para todo tipo de eventos, Instalamos y pagas al terminar el decorado!">
+
+
+    <title>¡Llenaremos de magia y color tu evento! Decoraciones temáticas con globos para todo tipo de eventos en CDMX </title>
+    <link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="assets/parallax/jarallax.css">
+    <link rel="stylesheet" href="assets/dropdown/css/style.css">
+    <link rel="stylesheet" href="assets/socicon/css/styles.css">
+    <link rel="stylesheet" href="assets/theme/css/style.css">
+    <link rel="stylesheet" href="assets/recaptcha.css">
+    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Spartan:wght@400;700&display=swap&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Spartan:wght@400;700&display=swap&display=swap">
+    </noscript>
+    <link rel="preload" href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap">
+    </noscript>
+    <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css">
+    <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    
+    <?php require_once "pixel.php";?>
+</head>
+
+<body>
+<?php require "menu.php";?>
+  
+    <section data-bs-version="5.1" class="image1 cid-t3Pj38lILr" id="image1-h">
+
+
+
+
+
+        <div>
+            <div class="row">
+                <div class="col-12 col-lg-6 col-md-12 image-wrap-1">
+                    <div class="image-wrapper">
+                        <img src="imgs/barbie/decoracion_barbie_1.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6 image-wrap-2">
+                    <div class="row">
+                        <div class="col-md-6 image-wrap">
+                            <div class="image-wrapper">
+                                <img src="imgs/barbie/decoracion_barbie_2.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-md-6 image-wrap">
+                            <div class="image-wrapper">
+                                <img src="imgs/barbie/decoracion_barbie_3.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-md-12 image-wrap">
+                            <div class="image-wrapper">
+                                <img src="imgs/barbie/decoracion_barbie_4.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section data-bs-version="5.1" class="slider1 mbr-embla cid-t3PhNQeo5q" id="slider1-a">
+
+
+
+        <div class="position-relative text-center">
+            <div class="col-12">
+
+                <h3 class="mbr-section-subtitle mbr-fonts-style display-3 text-uppercase color_accion d-md-block d-none">
+                    <strong>Dícelo con globos</strong>
+                </h3>
+
+                <h3 class="mbr-section-subtitle mbr-fonts-style display-7 text-uppercase color_accion d-md-none">
+                    <strong>Dícelo con globos</strong>
+                </h3>
+                <h2 class="mbr-section-title mbr-fonts-style display-3 text-uppercase">
+                    <strong>Tienes la idea, lo hacemos posible!</strong>
+                </h2>
+                <p class="mbr-text mbr-fonts-style display-7">
+                    <a class="btn btn-primary btn-lg btn-outline-dark" target="_blank" href="https://api.whatsapp.com/send?phone=+525576127281&text=Hola!%20Me%20puedes%20ayudar,%20me%20gustaría%20agendar%20mi%20evento">
+                        (55) 7612-7281
+                    </a>
+                </p>
+                <a class="color-black" target="_blank" href="https://api.whatsapp.com/send?phone=+525576127281&text=Hola!%20Me%20puedes%20ayudar,%20me%20gustaría%20agendar%20mi%20evento">
+                    <span class="fa fa-whatsapp fa-2x">
+                    </span>
+                    Agenda ya!
+                </a>
+            </div>
+            <div class="embla" data-skip-snaps="true" data-align="center" data-contain-scroll="trimSnaps" data-loop="true" data-auto-play-interval="5" data-draggable="true">
+                <div class="embla__viewport container-fluid">
+                    <div class="embla__container">
+                        <div class="embla__slide slider-image item" style="margin-left: 1rem; margin-right: 1rem;">
+                            <div class="card-wrap">
+                                <div class="item-wrapper position-relative">
+                                    <div class="image-wrap">
+                                        <img src="imgs/barbie/decoracion_barbie_5.jpg" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="embla__slide slider-image item" style="margin-left: 1rem; margin-right: 1rem;">
+                            <div class="card-wrap">
+                                <div class="item-wrapper position-relative">
+                                    <div class="image-wrap">
+                                        <img src="imgs/barbie/decoracion_barbie_6.jpg" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="embla__slide slider-image item" style="margin-left: 1rem; margin-right: 1rem;">
+                            <div class="card-wrap">
+                                <div class="item-wrapper position-relative">
+                                    <div class="image-wrap">
+                                        <img src="imgs/barbie/decoracion_barbie_7.jpg" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button class="embla__button embla__button--prev fa fa-chevron-circle-left fa-2x">
+
+
+                </button>
+                <button class="embla__button embla__button--next fa fa-chevron-circle-right fa-2x">
+
+
+                </button>
+            </div>
+        </div>
+    </section>
+
+    <section data-bs-version="5.1" class="features7 cid-t3PhwUgKcN galeria_seccion" id="features7-2">
+        <div class="container">
+            <div class="features-header">
+                <h3 class="mbr-section-subtitle mbr-fonts-style ">
+                    <strong>Ya sea una boda, un cumpleaños o cualquier festejo</strong>
+                </h3>
+                <h2 class="mbr-section-title mbr-fonts-style display-2 pink">
+                    <strong>¡Llenaremos de magia y color tu evento!</strong>
+                </h2>
+            </div>
+            <div class="row">
+                <div class="col-12 col-lg-6 col-md-6 card item features-image">
+                    <div class="item-wrapper">
+                        <div class="card-wrapper">
+                            <div class="">
+                                <img src="imgs/barbie/decoracion_barbie_8.jpg" alt="">
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6 col-md-6 card item features-image">
+                    <div class="item-wrapper">
+                        <div class="card-wrapper">
+                            <div class="">
+                                <img src="imgs/barbie/decoracion_barbie_9.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6 col-md-6 card item features-image">
+                    <div class="item-wrapper">
+                        <div class="card-wrapper">
+                            <div class="">
+                                <img src="imgs/barbie/decoracion_barbie_10.jpg" alt="">
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6 col-md-6 card item features-image">
+                    <div class="item-wrapper">
+                        <div class="card-wrapper">
+                            <div class="">
+                                <img src="imgs/barbie/decoracion_barbie_11.jpg" alt="">
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6 col-md-6 card item features-image">
+                    <div class="item-wrapper">
+                        <div class="card-wrapper">
+                            <div class="">
+                                <img src="imgs/barbie/decoracion_barbie_13.jpg" alt="">
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6 col-md-6 card item features-image">
+                    <div class="item-wrapper">
+                        <div class="card-wrapper">
+                            <div class="">
+                                <img src="imgs/barbie/decoracion_barbie_15.jpg" alt="">
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="mbr-section-btn">
+                <a class="btn btn-primary display-4" target="_blank" href="https://api.whatsapp.com/send?phone=+525576127281&text=Hola!%20Me%20puedes%20ayudar,%20me%20gustaría%20agendar%20mi%20evento">
+                    Checa disponibilidad!
+                </a>
+            </div>
+        </div>
+    </section>
+
+    <section data-bs-version="5.1" class="clients2 cid-t3PhAepAbm" id="clients2-5">
+
+        <div class="images-container container" id="preguntasfrecuentes">
+            <div class="row">
+                <div class="col-12 col-lg-6 mbr-section-head">
+                    <h2 class="mbr-section-title mbr-fonts-style display-2 preguntas">
+                        <strong>Lo que usualmente nos preguntan!</strong>
+                    </h2>
+                </div>
+                <div class="col-12 col-lg-6">
+                    <h3 class="color-black">
+                        ¿Cómo anticipo mi evento?
+                    </h3>
+                    <p class="mt-4">
+                        Para reservar la fecha de tu evento, es necesario realizar el pago anticipado del 10% de tu cotización y el monto total se liquidará
+                        al momento de terminar tu decoración
+                    </p>
+                    <h3 class="color-black mt-5">
+                        ¿Qué formas de pago aceptan?
+                    </h3>
+                    <p class="mt-4">
+                        En el momento que terminemos de realizar tu decoración podrás pagar con:
+                    </p>
+                    <p class="mt-4">
+                        - Efectivo
+                    </p>
+                    <p>
+                        - A través de transferencia
+                    </p>
+                    <p>
+                        - Pago con tarjeta de débido o crédito
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section data-bs-version="5.1" class="content1 cid-t3PiYeiBgz anticipa_ya" id="content1-g">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-12 content">
+                    <a class="mbr-text mbr-fonts-style display-5 border p-2" target="_blank" href="https://api.whatsapp.com/send?phone=+525576127281&text=Hola!%20Me%20puedes%20ayudar,%20me%20gustaría%20agendar%20mi%20evento">
+                        Anticipa la fecha de tu evento ya!
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+    
+
+    <section class="d-md-none" data-bs-version="5.1">
+        <div class="container-fluid col-md-8 col-mf-offsset-2">
+            <div class="fixed-bottom d-flex bg_blue">
+                <div class="mb-3 agenda">
+                    Agenda ya!
+                    <a class="color-black mr-3" target="_blank" href="https://api.whatsapp.com/send?phone=+525576127281&text=Hola!%20Me%20puedes%20ayudar,%20me%20gustaría%20agendar%20mi%20evento">
+                        <span class="fa fa-whatsapp fa-2x">
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/parallax/jarallax.js"></script>
+    <script src="assets/smoothscroll/smooth-scroll.js"></script>
+    <script src="assets/ytplayer/index.js"></script>
+    <script src="assets/dropdown/js/navbar-dropdown.js"></script>
+    <script src="assets/embla/embla.min.js"></script>
+    <script src="assets/embla/script.js"></script>
+    <script src="assets/sociallikes/social-likes.js"></script>
+    <script src="assets/theme/js/script.js"></script>
+    <script src="assets/formoid.min.js"></script>
+
+
+
+</body>
+
+</html>
+<style>
+    .bg_decoraciones {
+        background: #f01562 !important;
+
+    }
+
+    .color_accion {
+        color: #01024b !important;
+    }
+
+    .color-black {
+        color: #000000 !important;
+        font-size: 1.4em;
+        font-weight: bold;
+
+    }
+
+    .galeria_seccion {
+        background: #fcf2ff !important;
+    }
+
+    .bg_blue {
+        background: #e8b58a;
+    }
+
+    .agenda {
+        color: black !important;
+        margin-left: 210px;
+        font-weight: bold;
+    }
+
+    .color_white {
+        color: white !important;
+    }
+
+    .instalacion {
+        background: #fbbebd !important;
+    }
+
+    .costo {
+        color: #198754 !important;
+    }
+
+    .color_bbb {
+        color: #042b8c !important;
+    }
+
+    .preguntas {
+        color: #0c5c7e !important;
+    }
+
+    .anticipa_ya {
+        background: #ed0a83 !important;
+    }
+
+    .anticipa_ya_1 {
+        background: #c49248 !important;
+    }
+
+    .quince_a {
+
+        background: #000a0e !important;
+    }
+
+    .bg_pink {
+        background: #dca2b0 !important;
+    }
+
+    .amarillo {
+        background: #d8c3bd !important;
+    }
+
+    .decoraciones_4899 {
+        color: #03071a !important;
+    }
+
+    .color_black {
+        color: #020b18 !important;
+    }
+    .pink{
+        color:#eb11d5!important;
+    }
+</style>
