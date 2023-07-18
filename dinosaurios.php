b1287d3560d9f313f5642703e7af6546e0e5b52d
arihgrey@gmail.com <arihgrey@gmail.com>
2023-07-18 01:37:43 -0600

    [chore]

--- dinosaurios.php
+++ dinosaurios.php
@@ -223,40 +223,7 @@
    
 
     <section data-bs-version="5.1" class="clients2 cid-t3PhAepAbm" id="clients2-5">
-
-        <div class="images-container container">
-            <div class="row">
-                <div class="col-12 col-lg-6 mbr-section-head">
-                    <h2 class="mbr-section-title mbr-fonts-style display-2 preguntas">
-                        <strong>Lo que usualmente nos preguntan!</strong>
-                    </h2>
-                </div>
-                <div class="col-12 col-lg-6">
-                    <h3 class="color-black">
-                        ¿Cómo anticipo mi evento?
-                    </h3>
-                    <p class="mt-4">
-                        Para reservar la fecha de tu evento, es necesario realizar el pago anticipado del 10% de tu cotización y el monto total se liquidará
-                        al momento de terminar tu decoración
-                    </p>
-                    <h3 class="color-black mt-5">
-                        ¿Qué formas de pago aceptan?
-                    </h3>
-                    <p class="mt-4">
-                        En el momento que terminemos de realizar tu decoración podrás pagar con:
-                    </p>
-                    <p class="mt-4">
-                        - Efectivo
-                    </p>
-                    <p>
-                        - A través de transferencia
-                    </p>
-                    <p>
-                        - Pago con tarjeta de débido o crédito
-                    </p>
-                </div>
-            </div>
-        </div>
+        <?php require_once "preguntas_frecuentes.php";?>
     </section>
 
     <section data-bs-version="5.1" class="content1 cid-t3PiYeiBgz anticipa_ya" id="content1-g">
---
+++ preguntas_frecuentes.php
@@ -0,0 +1,33 @@
+<div class="images-container container" id="preguntasfrecuentes">
+    <div class="row">
+        <div class="col-12 col-lg-6 mbr-section-head">
+            <h2 class="mbr-section-title mbr-fonts-style display-2 preguntas">
+                <strong>Lo que usualmente nos preguntan!</strong>
+            </h2>
+        </div>
+        <div class="col-12 col-lg-6">
+            <h3 class="color-black">
+                ¿Cómo anticipo mi evento?
+            </h3>
+            <p class="mt-4">
+                Para reservar la fecha de tu evento, es necesario realizar el pago anticipado del 10% de tu cotización y el monto total se liquidará
+                al momento de terminar tu decoración
+            </p>
+            <h3 class="color-black mt-5">
+                ¿Qué formas de pago aceptan?
+            </h3>
+            <p class="mt-4">
+                En el momento que terminemos de realizar tu decoración podrás pagar con:
+            </p>
+            <p class="mt-4">
+                - Efectivo
+            </p>
+            <p>
+                - A través de transferencia
+            </p>
+            <p>
+                - Pago con tarjeta de débido o crédito
+            </p>
+        </div>
+    </div>
+</div>
