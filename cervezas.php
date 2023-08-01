e7cc728594a151be8f6bec7c1855e358a18c2282
arihgrey@gmail.com <arihgrey@gmail.com>
2023-08-01 03:04:37 -0600

    ]

---
+++ cervezas.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta property="og:image" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+    <meta name="description" content="¡Llenaremos de magia y color tu evento! Decoraciones temáticas con globos para todo tipo de eventos, Instalamos y pagas al terminar el decorado!">
+
+
+    <title>¡Llevamos la cerveza a tu evento CDMX! </title>
+    <link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="assets/parallax/jarallax.css">
+    <link rel="stylesheet" href="assets/dropdown/css/style.css">
+    <link rel="stylesheet" href="assets/socicon/css/styles.css">
+    <link rel="stylesheet" href="assets/theme/css/style.css">
+    <link rel="stylesheet" href="assets/recaptcha.css">
+    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Spartan:wght@400;700&display=swap&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Spartan:wght@400;700&display=swap&display=swap">
+    </noscript>
+    <link rel="preload" href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap">
+    </noscript>
+    <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css">
+    <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+
+    <?php require_once "pixel.php"; ?>
+</head>
+
+<body>
+    <?php require "menu.php"; ?>
+
+
+    <section data-bs-version="5.1" class="image1 cid-t3Pj38lILr" id="image1-h">
+
+        <div>
+            <div class="row">
+                <div class="col-12 col-lg-6 col-md-12 image-wrap-1">
+                    <div class="image-wrapper">
+                        <img src="imgs/cervezas/evento_cervezas.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6 image-wrap-2">
+                    <div class="row">
+                        <div class="col-md-6 image-wrap">
+                            <div class="image-wrapper">
+                                <img src="imgs/cervezas/evento_cervezas_1.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-md-6 image-wrap">
+                            <div class="image-wrapper">
+                                <img src="imgs/cervezas/evento_cervezas_2.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="col-md-12 image-wrap">
+                            <div class="image-wrapper">
+                                <img src="imgs/cervezas/evento_cervezas_4.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </d iv>
+            </div>
+    </section>
+
+    <section data-bs-version="5.1" class="slider1 mbr-embla cid-t3PhNQeo5q" id="slider1-a">
+
+
+
+        <div class="position-relative text-center">
+            <div class="col-12">
+
+                <h3 class="mbr-section-subtitle mbr-fonts-style display-3 text-uppercase black_accion d-md-block d-none">
+                    <strong>¡Te llevamos la cerveza!</strong>
+                </h3>
+
+                <h3 class="mbr-section-subtitle mbr-fonts-style display-7 text-uppercase black_accion d-md-none">
+                    <strong>¡Te llevamos la cerveza!</strong>
+                </h3>
+                <h2 class="mbr-section-title mbr-fonts-style display-3 text-uppercase">
+                    <strong>Para cumpleaños / Bautizos / Bodas / Reuniones </strong>
+                </h2>
+                <p class="mbr-text mbr-fonts-style display-7">
+                    <a class="btn btn-primary btn-lg btn-outline-dark" target="_blank" href="https://api.whatsapp.com/send?phone=+525576127281&text=Hola!%20Me%20puedes%20ayudar,%20me%20gustaría%20cotizar%20mi%20evento%20sobre%20las%20cervezas">
+                        (55) 7612-7281
+                    </a>
+                </p>
+                <a class="white" target="_blank" href="https://api.whatsapp.com/send?phone=+525576127281&text=Hola!%20Me%20puedes%20ayudar,%20me%20gustaría%20cotizar%20mi%20evento%20sobre%20las%20cervezas">
+                    <span class="fa fa-whatsapp fa-2x">
+                    </span>
+                    Cotiza tu evento!
+                </a>
+            </div>
+        </div>
+    </section>
+
+    <section class="d-md-none" data-bs-version="5.1">
+        <div class="container-fluid col-md-8 col-mf-offsset-2">
+            <div class="fixed-bottom d-flex bg_blue">
+                <div class="mb-3 agenda">
+                    Cotiza tu evento!
+                    <a class="white mr-3" target="_blank" href="https://api.whatsapp.com/send?phone=+525576127281&text=Hola!%20Me%20puedes%20ayudar,%20me%20gustaría%20cotizar%20mi%20evento%20sobre%20las%20cervezas">
+                        <span class="fa fa-whatsapp fa-2x">
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/parallax/jarallax.js"></script>
+    <script src="assets/smoothscroll/smooth-scroll.js"></script>
+    <script src="assets/ytplayer/index.js"></script>
+    <script src="assets/dropdown/js/navbar-dropdown.js"></script>
+    <script src="assets/embla/embla.min.js"></script>
+    <script src="assets/embla/script.js"></script>
+    <script src="assets/sociallikes/social-likes.js"></script>
+    <script src="assets/theme/js/script.js"></script>
+    <script src="assets/formoid.min.js"></script>
+
+
+
+</body>
+
+</html>
+<style>
+    .bg_decoraciones {
+        background: #f01562 !important;
+
+    }
+
+
+    .color-black {
+        color: #000000 !important;
+        font-size: 1.4em;
+        font-weight: bold;
+
+    }
+
+    .galeria_seccion {
+        background: #020226 !important;
+    }
+
+    .bg_blue {
+        background: #e8b58a;
+    }
+
+    .agenda {
+        color: black !important;
+        margin-left: 210px;
+        font-weight: bold;
+    }
+
+    .color_white {
+        color: white !important;
+    }
+
+    .instalacion {
+        background: #fbbebd !important;
+    }
+
+    .costo {
+        color: #198754 !important;
+    }
+
+    .color_bbb {
+        color: #042b8c !important;
+    }
+
+    .preguntas {
+        color: #0c5c7e !important;
+    }
+
+    .anticipa_ya {
+        background: #ed0a83 !important;
+    }
+
+    .anticipa_ya_1 {
+        background: #c49248 !important;
+    }
+
+    .quince_a {
+
+        background: #000a0e !important;
+    }
+
+    .bg_pink {
+        background: #dca2b0 !important;
+    }
+
+    .amarillo {
+        background: #d8c3bd !important;
+    }
+
+    .decoraciones_4899 {
+        color: #03071a !important;
+    }
+
+    .color_black {
+        color: #020b18 !important;
+    }
+
+    .precio {
+        background: #f8babc;
+        padding: 10px;
+        font-weight: bold;
+        color: black !important;
+
+    }
+
+    .precio_2 {
+        background: black;
+        padding: 10px;
+        color: white;
+
+    }
+
+    .precio_3 {
+        background: #ba7c6c;
+        padding: 10px;
+        color: white;
+    }
+
+    .mas_detalles {
+        color: black;
+    }
+
+    .cid-t3PhNQeo5q {
+        background: #f60505 !important;
+    }
+
+    .black_accion {
+        color: #000000 !important;
+    }
+</style>
--- menu.php
+++ menu.php
@@ -1,78 +1,89 @@
 <section data-bs-version="5.1" class="menu bistro_menu1 cid-t3PhvdPAtM" once="menu" id="menu1-0">
-        <nav class="navbar navbar-dropdown navbar-expand-lg">
-            <div class="menu_box container">
-                <div class="navbar-brand d-flex d-lg-none">
-                    <span class="navbar-caption-wrap"><a class="navbar-caption display-7" href="index.php">Globolandia</a></span>
+    <nav class="navbar navbar-dropdown navbar-expand-lg">
+        <div class="menu_box container">
+            <div class="navbar-brand d-flex d-lg-none">
+                <span class="navbar-caption-wrap"><a class="navbar-caption display-7" href="index.php">Globolandia</a></span>
+            </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse" data-target="#navbarSupportedContent" data-bs-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <div class="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
                 </div>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse" data-target="#navbarSupportedContent" data-bs-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <div class="hamburger">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <div class="navbar-brand d-none d-lg-flex">
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="navbar-brand d-none d-lg-flex">
 
-                        <span class="navbar-caption-wrap"><a class="navbar-caption display-5" href="index.php">Globolandia</a></span>
-                    </div>
-                    <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
-                        <!--<li class="nav-item"><a class="nav-link link display-4" href="index.php#preguntasfrecuentes">Preguntas frecuentes</a></li>                        -->
-                        <li class="nav-item"><a class="nav-link link display-4" href="#features9sss-3">(55) 7612- 7281 </a></li>
-                        <li class="nav-item">
+                    <span class="navbar-caption-wrap"><a class="navbar-caption display-5" href="index.php">Globolandia</a></span>
+                </div>
+                <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
+                    
+                    <li class="nav-item"><a class="nav-link link display-4" href="#features9sss-3">(55) 7612- 7281 </a></li>
+                    <li class="nav-item">
                         <a class="nav-link dropdown-toggle display-4" href="#" id="tematicasDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Temáticas
+                            Temáticas populares
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="tematicasDropdown">
-                        <li><a class="dropdown-item" href="barbie.php">Barbie</a></li>                            
-                            <li><a class="dropdown-item" href="babyshowers.php">Baby shower</a></li>                                                        
-                            <li><a class="dropdown-item" href="quinces.php">Quince Años</a></li> 
-                            <li><a class="dropdown-item" href="infantiles.php">Infantiles</a></li>                                                        
-                            <li><a class="dropdown-item" href="https://enidservices.com/decoraciones-tematicas-globolandia/">Todos los modelos!</a></li>                                                                                    
+                            <li><a class="dropdown-item" href="barbie.php">Barbie</a></li>
+                            <li><a class="dropdown-item" href="babyshowers.php">Baby shower</a></li>
+                            <li><a class="dropdown-item" href="quinces.php">Quince Años</a></li>
+                            <li><a class="dropdown-item" href="infantiles.php">Infantiles</a></li>
+                            <li><a class="dropdown-item" href="https://enidservices.com/decoraciones-tematicas-globolandia/">Todos los modelos!</a></li>
                         </ul>
                     </li>
-                    </ul>
-                    <div class="offcanvas_box">
-                        <button class="btn_offcanvas" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
-                            <div class="hamburger">
-                                <span></span>
-                                <span></span>
+                    <li class="nav-item">
+                        <a class="nav-link dropdown-toggle display-4" href="#" id="tematicasDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Servicios
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="tematicasDropdown">
+                            <li><a class="dropdown-item" href="index.php">Decoraciones temáticas</a></li>
+                            <li><a class="dropdown-item" href="cervezas.php">Cervezas a domicilio pago contra entrega</a></li>
+                            
+                        </ul>
+                    </li>
+
+                </ul>
+                <div class="offcanvas_box">
+                    <button class="btn_offcanvas" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+                        <div class="hamburger">
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </button>
+                    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                        <div class="offcanvas-header">
+                            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body">
+
+                            <div class="offcanvas_contact">
+                                <h3 class="mbr-section-subtitle mbr-fonts-style display-5"><strong>Globolandia.</strong></h3>
                             </div>
-                        </button>
-                        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-                            <div class="offcanvas-header">
-                                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                            <div class="icons-menu">
+                                <a class="iconfont-wrapper" href="https://www.facebook.com/profile.php?id=100093599380757" target="_blank">
+                                    <span class="p-2 mbr-iconfont socicon-facebook socicon"></span>
+                                    <p class="mbr-text mbr-fonts-style display-4">
+                                        Facebook</p>
+                                </a>
+
+                                <a class="iconfont-wrapper" href="https://www.instagram.com/globos_globolandia_/" target="_blank">
+                                    <span class="p-2 mbr-iconfont socicon-instagram socicon"></span>
+                                    <p class="mbr-text mbr-fonts-style display-4">
+                                        Instagram</p>
+                                </a>
                             </div>
-                            <div class="offcanvas-body">
+                            <p class="text_widget mb-0 mbr-fonts-style display-4">
+                                <br><br>
+                                <a class="text-secondary">(55) 7612-7281</a>
+                                <br><br>
 
-                                <div class="offcanvas_contact">
-                                    <h3 class="mbr-section-subtitle mbr-fonts-style display-5"><strong>Globolandia.</strong></h3>
-                                </div>
-                                <div class="icons-menu">
-                                    <a class="iconfont-wrapper" href="https://www.facebook.com/profile.php?id=100093599380757" target="_blank">
-                                        <span class="p-2 mbr-iconfont socicon-facebook socicon"></span>
-                                        <p class="mbr-text mbr-fonts-style display-4">
-                                            Facebook</p>
-                                    </a>
+                            </p>
 
-                                    <a class="iconfont-wrapper" href="https://www.instagram.com/globos_globolandia_/" target="_blank">
-                                        <span class="p-2 mbr-iconfont socicon-instagram socicon"></span>
-                                        <p class="mbr-text mbr-fonts-style display-4">
-                                            Instagram</p>
-                                    </a>
-                                </div>
-                                <p class="text_widget mb-0 mbr-fonts-style display-4">
-                                    <br><br>
-                                    <a class="text-secondary">(55) 7612-7281</a>
-                                    <br><br>
-
-                                </p>
-
-                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </nav>
-    </section>
+        </div>
+    </nav>
+</section>
